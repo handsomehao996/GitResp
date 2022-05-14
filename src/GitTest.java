6fb58dcfe999d349cb1eccf9940ed7adf799b2bc
hao <1812677782@qq.com>
2022-05-14 15:21:13 +0800

    克隆成功，添加个方法

--- src/GitTest.java
+++ src/GitTest.java
@@ -7,4 +7,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Play the git");
     }
+
+    public static void SayHello(){
+        System.out.println("hahahahahaha");
+    }
 }
